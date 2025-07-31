df0c12222ce2b5c4b63f59b27736b0951f902cfc
CryptoCapi <pnombre26@gmail.com>
2025-07-31 11:20:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- // Scroll animations
-       const observerOptions = {
+   // Scroll animations
+        const observerOptions = {
             threshold: 0.1,
             rootMargin: '0px 0px -50px 0px'
         };
